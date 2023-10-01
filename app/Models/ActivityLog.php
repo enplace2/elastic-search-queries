8c6855734e6b93349089857defb775e44ac5f09d
Nick Place <114956247+enplace2@users.noreply.github.com>
2023-10-01 00:27:10 -0300

    create activity models

---
+++ app/Models/ActivityLog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityLog extends Model
+{
+    use HasFactory;
+    protected $table = 'activity_logs';
+
+    public function activityTypes(){
+        return $this->belongsto(ActivityType::class);
+    }
+}
